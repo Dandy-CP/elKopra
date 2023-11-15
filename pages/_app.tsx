5ba959650b07737a9004b9284135850cdba0e813
Dandy Candra <dandycandrapratama@gmail.com>
2023-11-15 22:59:40 +0700

    add queryClientProvider and Layout to root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import "@mantine/core/styles.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { MantineProvider } from "@mantine/core";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/config/queryClient";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
-    <MantineProvider>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        {router.pathname !== "/_error" ? (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
